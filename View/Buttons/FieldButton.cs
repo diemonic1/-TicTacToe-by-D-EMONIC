c4b890bb90b7550c31c2331c8731084551172d81
diemonic1 <diemonic1@gmail.com>
2022-08-08 20:20:14 +0300

    Update FieldButton.cs

--- View/Buttons/FieldButton.cs
+++ View/Buttons/FieldButton.cs
@@ -1,9 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FieldButton : ButtonRoot
 {
     [SerializeField] private int _buttonNumber;
+
+    public FieldButton()
+    {
+        if (_buttonNumber < 0 || _buttonNumber > 8)
+            throw new Exception("The button number on the field must be from 0 to 8!");
+    }
 
     public delegate void ButtonHandler(int numberOfActivated);
 
